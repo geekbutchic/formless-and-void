6c0b65c6f5b5fdf2e3eac9f2a979ee50cbc07a05
Timothy Negron <timothy.negron@codeimmersives.com>
2021-02-10 13:31:28 -0500

    Adds javascript to select tag

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ document.querySelector("button").addEventListener("click", () => {
     document.querySelector("h1." + input.id).innerText = input.value;
   });
 });
+
+document.querySelector("button").addEventListener("click", () => {
+  document.querySelectorAll("select").forEach((select) => {
+    document.querySelector("h1." + select.id).innerText = select.value;
+  });
+});
